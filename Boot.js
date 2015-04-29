6faeaac13a2e86b00d115bbf2b53f2af27d89de1
JQSH <ecjosh@gmail.com>
2015-04-30 04:42:44 +1000

    Increase game size

--- Boot.js
+++ Boot.js
@@ -3,7 +3,7 @@
 'use strict';
 
 var BasicGame = {
-    pixel: { scale: 3, canvas: null, context: null, width: 0, height: 0 },
+    pixel: { scale: 5, canvas: null, context: null, width: 0, height: 0 },
 };
 
 
